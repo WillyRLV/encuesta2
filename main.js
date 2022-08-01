763161106df8d0303525d434b07cf489947c23b8
william Luis <williamlv1998@gmail.com>
2022-08-01 13:48:20 -0500

    avance 1_8

--- main.js
+++ main.js
@@ -1,5 +1,5 @@
 var form = document.forms['form']
-const input1=document.getElementById("name")
+const input1 = document.getElementById("name")
 const valido = document.getElementById("valid")
 
 
@@ -7,22 +7,39 @@ form.onsubmit = function (e) {
     e.preventDefault();
     var select = document.form.name.value;
     console.log(select);
+    var select = document.getElementById("validationCustom04"),
+    arr = ["juan", "jorge", "luis"];
 
-    if (document.getElementById("name").value === '36756730') {
-        document.getElementById("form2").style.display = "inline-block";
+
+
+    if (document.getElementById("name").value === '33333333') {
+
+    
+
+        for (let index = 0; index < arr.length; index++) {
+
+            const op = document.createElement("OPTION"),
+                txt = document.createTextNode(arr[index]);
+            op.appendChild(txt);
+            op.setAttribute("value", arr[index])
+            select.insertBefore(op, select.lastChild);
+
+        }
+
+        document.getElementById("form2").style.display = "flex";
 
     }
 
-    else{
+    else {
         document.getElementById("valid").style.display = "inline-block";
     }
 
 }
 
 input1.addEventListener('change', (event) => {
-        document.getElementById("form2").style.display = "none";
-        document.getElementById("valid").style.display = "none";
-    
+    document.getElementById("form2").style.display = "none";
+    document.getElementById("valid").style.display = "none";
+
 });
 
 
@@ -30,7 +47,7 @@ input1.addEventListener('keydown', (event) => {
 
     document.getElementById("valid").style.display = "none";
     document.getElementById("form2").style.display = "none";
-    
+
 });
 
 
